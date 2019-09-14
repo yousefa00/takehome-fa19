b37d64c7572e82b9759ba2ca844b29aed4013f6d
Yousef Ahmed <yousefahmed0066@gmail.com>
2019-09-14 06:20:19 -0500

    Added complete prop into Instructions component, part 1 done!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Instructions />
+        <Instructions complete={true}/>
 
         {this.state.contacts.map(x => (
           <Contact id={x.id} name={x.name} nickcname={x.nickname} hobby={x.hobby} />
